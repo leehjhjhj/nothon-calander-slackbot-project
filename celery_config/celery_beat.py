8fc2dd25518196659832e2f45cf2335e14c3bed4
leehjhjhj <tsukiakarii@naver.com>
2023-10-01 07:51:39 +0900

    faet: 크론탭으로 변경

--- celery_config/celery_beat.py
+++ celery_config/celery_beat.py
@@ -1,11 +1,11 @@
 from .celery_app import celery_task
-from datetime import timedelta
 from service.save_meeting_facade import save_meeting_facade
+from celery.schedules import crontab 
 
 celery_task.conf.beat_schedule = {
     'schedule-meetings-every-hour': {
         'task': 'celery_config.celery_beat.read_calander_schedule',
-        'schedule': timedelta(minutes=20),
+        'schedule': crontab(minute=10)
     },
 }
 
