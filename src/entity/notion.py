8ae46c78742bf67a50e2cc5cd3f0bab58a487131
leehjhjhj <tsukiakarii@naver.com>
2023-10-27 15:19:28 +0900

    chore: 배포를 위한 workdir추가와database 모듈 경로 수정

--- src/entity/notion.py
+++ src/entity/notion.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
-from database import Base
+from src.database import Base
 from datetime import datetime
 import enum
 from sqlalchemy.orm import relationship
--- src/entity/notion_slack_mapping.py
+++ src/entity/notion_slack_mapping.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from database import Base
+from src.database import Base
 from sqlalchemy.orm import relationship
 
 class NotionSlackMapping(Base):
