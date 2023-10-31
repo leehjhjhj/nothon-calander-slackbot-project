02c90837b4c5ba7c6214f89091b602a0f4cb4061
leehjhjhj <tsukiakarii@naver.com>
2023-10-31 10:18:43 +0900

    feat: 기존 유저아이디 로직 레디스로 수정

--- src/api/service/farthing_patifipants.py
+++ src/api/service/farthing_patifipants.py
@@ -1,7 +1,8 @@
-from .workspace import Workspace
+from utils.redis_connect import get_redis_connection
 from database import SessionLocal
 from api.persistance import SlackRepository
 import logging
+import json
 
 def make_participants(result, notion_database_id):
     slack_repo = SlackRepository(db=SessionLocal())
@@ -11,10 +12,11 @@ def make_participants(result, notion_database_id):
         participants_names = [participants_info['name'] for participants_info in participants_infos]
         if not participants_names:
             return False
-        slack_token = slack_repo.get_api_token_by_notion_database_id(notion_database_id)
-        workspace = Workspace(token=slack_token)
-        logging.info('users.list api 호출')
-        name_user_id_map = workspace.get_users()
+        
+        slack_channel_id = slack_repo.get_slack_channel_id_by_notion_database_id(notion_database_id)
+        redis_conn = get_redis_connection(db_select=3)
+        name_user_id_map_byte = redis_conn.get(f"{slack_channel_id}")
+        name_user_id_map = json.loads(name_user_id_map_byte)
 
         for participants_name in participants_names:
             user_id = name_user_id_map.get(f'{participants_name}', False)
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from src.api.service.redis_user_lists import save_user_lists_to_redis
-app = FastAPI()
-
-
-@app.get("/")
-async def save_user_lists_to_redis():
-    pass
