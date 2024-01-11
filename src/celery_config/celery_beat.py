049fa10c841f7dd0a467e54de86e849fe6e71f3a
leehjhjhj <tsukiakarii@naver.com>
2024-01-11 15:47:21 +0900

    refactor: 비트 시간 동적화

--- src/celery_config/celery_beat.py
+++ src/celery_config/celery_beat.py
@@ -1,11 +1,16 @@
 from .celery_app import celery_task
 from datetime import timedelta 
 from api.service.save_meeting_facade import save_meeting_facade
+from decouple import config
+
+SCHEDULE_TIME_UNITS = config('SCHEDULE_TIME_UNITS')
+SCHEDULE_PARAMETER = config('SCHEDULE_PARAMETER')
+print(SCHEDULE_TIME_UNITS, SCHEDULE_PARAMETER)
 
 celery_task.conf.beat_schedule = {
     'schedule-meetings-every-hour': {
         'task': 'celery_config.celery_beat.schedule_meeting',
-        'schedule': timedelta(hours=1)
+        'schedule': timedelta(**{SCHEDULE_TIME_UNITS: int(SCHEDULE_PARAMETER)})
     },
 }
 
