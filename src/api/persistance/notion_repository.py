23d2f3010eb968553169c3a6161f879890af8b46
leehjhjhj <tsukiakarii@naver.com>
2024-01-13 11:28:40 +0900

    refactor: repository db 주입 방식 변경

--- src/api/persistance/notion_repository.py
+++ src/api/persistance/notion_repository.py
@@ -1,21 +1,19 @@
-from sqlalchemy.orm import Session
 from entity import Notion, NotionDatabase
 from sqlalchemy.orm import joinedload
+from database import get_db
 
 class NotionRepository:
-    
-    def __init__(self, db: Session):
-        self.db = db
 
     def get_api_token_by_notion_database_id(self, notion_database_id):
-        notion_database = (
-            self.db.query(NotionDatabase)
-            .options(joinedload(NotionDatabase.notion))
-            .filter_by(notion_database_id=notion_database_id)
-            .first()
-        )
+        with get_db() as db:
+            notion_database = (
+                db.query(NotionDatabase)
+                .options(joinedload(NotionDatabase.notion))
+                .filter_by(notion_database_id=notion_database_id)
+                .first()
+            )
 
-        if notion_database and notion_database.notion:
-            return notion_database.notion.notion_api_token
-        else:
-            return None
+            if notion_database and notion_database.notion:
+                return notion_database.notion.notion_api_token
+            else:
+                return None
--- src/api/persistance/notion_slack_mapping_repository.py
+++ src/api/persistance/notion_slack_mapping_repository.py
@@ -1,13 +1,12 @@
-from sqlalchemy.orm import Session
 from entity import NotionSlackMapping
+from database import get_db
 
 class NotionSlackMappingRepository:
-    
-    def __init__(self, db: Session):
-        self.db = db
 
     def get_all_database_ids(self):
-        return [record[0] for record in self.db.query(NotionSlackMapping.notion_database_id).distinct().all()]
+        with get_db() as db:
+            return [record[0] for record in db.query(NotionSlackMapping.notion_database_id).distinct().all()]
     
     def get_slack_channel_id_by_notion_database_id(self, notion_database_id):
-        return [m.slack_channel_id for m in self.db.query(NotionSlackMapping).filter_by(notion_database_id=notion_database_id)] 
+        with get_db() as db:
+            return [m.slack_channel_id for m in db.query(NotionSlackMapping).filter_by(notion_database_id=notion_database_id)] 
