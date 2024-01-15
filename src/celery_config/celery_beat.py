ccc1bdf3abab1b05a4c2e4d83b5a0e79780eee7d
leehjhjhj <tsukiakarii@naver.com>
2024-01-15 09:41:11 +0900

    feat: participants DI 추가

--- src/celery_config/celery_beat.py
+++ src/celery_config/celery_beat.py
@@ -1,16 +1,19 @@
 from .celery_app import celery_task
 from datetime import timedelta 
-from .containers import MeetingContainer
+from api.service.containers import MeetingContainer
 from decouple import config
 
 SCHEDULE_TIME_UNITS = config('SCHEDULE_TIME_UNITS')
 SCHEDULE_PARAMETER = config('SCHEDULE_PARAMETER')
-print(SCHEDULE_TIME_UNITS, SCHEDULE_PARAMETER)
 
 celery_task.conf.beat_schedule = {
     'schedule-meetings-every-hour': {
         'task': 'celery_config.celery_beat.schedule_meeting',
         'schedule': timedelta(**{SCHEDULE_TIME_UNITS: int(SCHEDULE_PARAMETER)})
+    },
+    'schedule-redis_user_list-every-day': {
+        'task': 'celery_config.celery_beat.schedule_redis_user_list',
+        'schedule': timedelta(days=1)
     },
 }
 
@@ -18,3 +21,8 @@ celery_task.conf.beat_schedule = {
 def schedule_meeting():
     meeting_process = MeetingContainer.meeting_process()
     meeting_process.save_meeting()
+
+@celery_task.task
+def schedule_redis_user_list():
+    participants_process = MeetingContainer.participants_process()
+    participants_process.save_user_lists_to_redis()
