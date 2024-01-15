092c4cb237814cd9529f34460baa22b9f2569dde
leehjhjhj <tsukiakarii@naver.com>
2024-01-15 09:40:27 +0900

    feat: 레디스 유저저장 관련 레포 로직 추가

--- src/api/persistance/slack_repository.py
+++ src/api/persistance/slack_repository.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import joinedload
 from database import get_db
 
 class SlackRepository:
+    def get_slack_channel_id_by_notion_database_id(self, notion_database_id):
+        with get_db() as db:
+            return db.query(NotionSlackMapping.slack_channel_id).filter_by(notion_database_id=notion_database_id).scalar()
 
     def get_api_token_by_slack_channel_id(self, slack_channel_id):
         with get_db() as db:
@@ -12,7 +15,7 @@ class SlackRepository:
                 .filter_by(slack_channel_id=slack_channel_id)
                 .first()
             )
-
+            
             if slack_channel and slack_channel.slack:
                 return slack_channel.slack.slack_api_token
             else:
