008c99c1b0172e0b9bc5941b3d98466a1ab91b48
leehjhjhj <tsukiakarii@naver.com>
2024-01-15 09:40:05 +0900

    feat: 레디스 유저저장 로직 추가

--- src/api/service/participants_process.py
+++ src/api/service/participants_process.py
@@ -1,16 +1,31 @@
 import logging
 from entity import NotionPage
 from slack_sdk import WebClient
+from utils.redis_connect import get_cache_redis_connection
+import json
 
 class ParticipantsProcess:
-    def __init__(self, slack_repository):
+    def __init__(self, slack_repository, notion_slack_mapping_repository):
         self._slack_repo = slack_repository
+        self._notion_slack_mapping_repo = notion_slack_mapping_repository
+        self._redis_conn = get_cache_redis_connection(db_select=1)
 
     def add_participants(self, meeting: NotionPage, result) -> NotionPage:
         participants = self._make_participants(result, meeting.notion_database_id)
         meeting.participants = participants if participants else None
         return meeting
-    
+            
+    def save_user_lists_to_redis(self):
+        slack_ids = self._notion_slack_mapping_repo.get_all_slack_channels()
+        for slack_id in slack_ids:
+            try:
+                slack_token = self._slack_repo.get_api_token_by_slack_channel_id(slack_id)
+                name_user_id_map = self._get_slack_users(slack_token)
+                name_user_id_bytes = json.dumps(name_user_id_map)
+                self._redis_conn.set(f"{slack_id}", name_user_id_bytes)
+            except Exception as e:
+                logging.error(f"레디스 유저리스트 저장 오류 발생: {e}")
+
     def _make_participants(self, result, notion_database_id):
         try:
             mention = ''
@@ -18,9 +33,10 @@ class ParticipantsProcess:
             participants_names = [participants_info['name'] for participants_info in participants_infos]
             if not participants_names:
                 return False
-            slack_token = self._slack_repo.get_api_token_by_notion_database_id(notion_database_id)
-            logging.info('users.list api 호출')
-            name_user_id_map = self._get_slack_users(slack_token)
+
+            slack_channel_id = self._slack_repo.get_slack_channel_id_by_notion_database_id(notion_database_id)
+            name_user_id_map_byte = self._redis_conn.get(f"{slack_channel_id}")
+            name_user_id_map = json.loads(name_user_id_map_byte)
 
             for participants_name in participants_names:
                 user_id = name_user_id_map.get(f'{participants_name}', False)
