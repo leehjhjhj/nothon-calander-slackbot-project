3469fdb740d331847e74f2cb235979bf3f3c805a
leehjhjhj <tsukiakarii@naver.com>
2024-01-15 09:39:23 +0900

    feat: 캐시용 레디스 접속 추가

---
+++ src/utils/redis_connect.py
@@ -0,0 +1,17 @@
+from decouple import config
+from redis import Redis
+import logging
+
+def get_cache_redis_connection(db_select: int):
+    redis_host = config('REDIS_CACHE_HOST')
+    redis_port = config('REDIS_CACHE_PORT')
+    redis_con = Redis(host=redis_host, port=redis_port, db=1)
+
+    try:
+        if not redis_con.ping():
+            return None
+    except Exception as e:
+        logging.error(f'레디스 연결 오류: {e}')
+        return None
+
+    return redis_con
