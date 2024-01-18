dda2daa4702d8e661ac2935dde0fab577939d3a7
leehjhjhj <tsukiakarii@naver.com>
2024-01-18 11:15:26 +0900

    HOTFIX: 셀러리 앱 변수화

--- src/celery_config/celery_app.py
+++ src/celery_config/celery_app.py
@@ -2,11 +2,12 @@ from celery import Celery
 from decouple import config
 
 redis_host = config('REDIS_HOST')
+redis_port = config('REDIS_PORT')
 
 celery_task = Celery(
     'app',
-    broker=f"redis://{redis_host}:6379/0",
-    backend=f"redis://{redis_host}:6379/1",
+    broker=f"redis://{redis_host}:{redis_port}/0",
+    backend=f"redis://{redis_host}:{redis_port}/1",
     include=['celery_config.celery_beat', 'celery_config.celery_worker']
 )
 
